0d605fe5b9923b984171037ceb4c2ab9e1c5473b
Zarrouk  Hamadi <100397989+Hamadi-zrk@users.noreply.github.com>
2022-03-13 21:12:31 +0100

    !

--- app.py
+++ app.py
@@ -7,19 +7,25 @@ the results in JSON format.
 
 # Import libraries
 import numpy as np
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import pickle
 import pandas as pd
-import jsonify
-    
+import json
+
 app = Flask(__name__)
 
-@app.route('/load_models', methods=['get'])
 def load_models():
     model = pickle.load(open('models/LRModel2.obj','rb'))
     return model
 
-@app.route('/predict', methods=['POST']) 
+@app.route("/")
+def hello():
+    """
+    Ping the API.
+    """
+    return jsonify({"text":"Hello, the API is up and running..." })
+
+@app.route('/predict', methods=['POST'])
 def predict():
     # parse input features from request
     request_json = request.get_json()
@@ -31,15 +37,9 @@ def predict():
     # Format prediction in percentage with 2 decimal points
     prediction = "The client has a " + str(round(prediction*100,2)) + "% risk of defaulting on their loan."
     print("prediction: ", prediction)
+
     # Return output
-    return jsonify((str(prediction)) 
+    return jsonify(str(prediction))
 
-@app.route("/") 
-def hello():
-    """
-    Ping the API.
-    """
-    return jsonify({"text":"Prediction de api-hamadi..." },str (prediction))
-
-if _name_ == "_main_":
-    app.run
+if __name__ == '__main__':
+    app.run(debug=True)
